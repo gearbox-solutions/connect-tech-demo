e5c329f37b0be77fe606e88db6c6ec3a735340ed
David Nahodyl <david@gearboxgo.com>
2023-10-25 00:14:43 -0400

    disabled devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["~/assets/app.css"],
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   image: {
     dir: "assets/images/",
   },
